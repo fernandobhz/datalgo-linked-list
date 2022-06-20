efd2386b149bdd4ad3868296d908c45bdb6441ac
Fernando Reis Guimarães <fernandobhz@gmail.com>
2022-06-20 01:37:55 -0300

    chore: insert and printList

--- index.mjs
+++ index.mjs
@@ -26,21 +26,54 @@ class LinkedList {
     this.head = newHead;
     this.length++;
   }
+  insert(index, value) {
+    if (index < 1) {
+      throw new Error(`Use prepend instead`);
+    }
+        
+    let currentIndex = 0;
+    let previousNode = this.head;
+    let currentNode = this.head.next;
+
+    while(currentIndex < index - 1) {
+      previousNode = currentNode;
+      currentNode = currentNode.next;
+      currentIndex++;
+    }
+
+    const newNode = {
+      value,
+      next: currentNode
+    }
+
+    previousNode.next = newNode;
+  }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+
+    while(currentNode.next !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+
+    console.log(array.join(' > '));
+  }
 }
+// 1 > 10 > 5 > 16
+// 1 > 10 > 99 > 5 > 16
 
 let myLinkedList = new LinkedList(10);
-console.log(JSON.stringify(myLinkedList));
+myLinkedList.printList();
 
 myLinkedList.append(5);
-console.log(JSON.stringify(myLinkedList));
+myLinkedList.printList();
 
 myLinkedList.append(16);
-console.log(JSON.stringify(myLinkedList));
+myLinkedList.printList();
 
 myLinkedList.prepend(1);
-console.log(JSON.stringify(myLinkedList));
+myLinkedList.printList();
 
-
-
-
-
+myLinkedList.insert(2, 99);
+myLinkedList.printList();
